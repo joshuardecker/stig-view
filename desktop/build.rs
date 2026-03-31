699b9ca80f169e7ceaaba3fcab05a11079154383
joshuardecker <joshuardecker@pm.me>
2026-03-31 16:05:07 -0600

    Trying new ico file.

--- desktop/build.rs
+++ desktop/build.rs
@@ -1,13 +1,7 @@
 fn main() {
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default() == "windows" {
-        let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-        let icon_path = std::path::Path::new(&manifest_dir)
-            .parent()
-            .unwrap()
-            .join("assets/icon.ico");
-
         let mut res = winresource::WindowsResource::new();
-        res.set_icon(icon_path.to_str().unwrap());
+        res.set_icon("../../assets/io.github.joshuardecker.stig-view.ico");
         res.compile().unwrap();
     }
 }
